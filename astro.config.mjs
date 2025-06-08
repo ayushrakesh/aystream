21dcbd5a24e8a08e0fa45ead3efd0858696cfd4d
ayushrakesh <ayushrakesh.05@gmail.com>
2025-06-08 10:34:08 +0530

    removed vite config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
 import robotsTxt from "astro-robots-txt"
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://aystream.onrender.com',
+  vite: {
+    preview: {
+      host: true,
+      port: parseInt(process.env.PORT) || 4321,
+      allowedHosts: ['aystream.onrender.com']
+    }
+  }
 })
--- vite.config.mjs
+++
@@ -1,5 +0,0 @@
-export default {
-    preview: {
-      allowedHosts: ['aystream.onrender.com'],
-    },
-};
